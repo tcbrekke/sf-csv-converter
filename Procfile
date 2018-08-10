7e264ed93cac1de5e5ee6bfdaf5ecefecac04fe9
Thomas Brekke <tcbrekke@gmail.com>
2018-08-10 10:58:00 -0700

    add flask app, procfile, etc.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cal_converter:app
---
+++ cal_converter.py
@@ -0,0 +1,36 @@
+# Dependencies
+#------------------------
+from flask import Flask, render_template
+import pandas as pd 
+import os
+import sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+# Establish Database Connection
+#------------------------
+
+DATABASE_URL = os.environ['DATABASE_URL']
+engine = create_engine(DATABASE_URL)
+connection = engine.connect()
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+session = Session(engine)
+
+# Flask Routes
+#------------------------
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("upload.html", methods = ['POST'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
